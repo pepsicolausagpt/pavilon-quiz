5326792b1d1802215f1b095b46fa2985d3873095
pepsicolausagpt <pepsicolausagpt@users.noreply.github.com>
2026-05-14 15:16:47 +0300

    Fix image paths for GitHub Pages

--- src/data/pavilions.js
+++ src/data/pavilions.js
@@ -1,11 +1,11 @@
 export const pavilions = [
-  { id: "ultraclassic", name: "Ultraclassic", price: "от 240 000 руб.", image: "/images/ultraclassic.jpg" },
-  { id: "carla_space", name: "Carla Space", price: "от 245 000 руб.", image: "/images/carlaspace.jpg" },
-  { id: "elefante", name: "Elefante", price: "от 565 000 руб.", image: "/images/elefante.jpg" },
-  { id: "eleonora", name: "Eleonora", price: "от 520 000 руб.", image: "/images/eleonora.jpg" },
+  { id: "ultraclassic", name: "Ultraclassic", price: "от 240 000 руб.", image: "/pavilon-quiz/images/ultraclassic.jpg" },
+  { id: "carla_space", name: "Carla Space", price: "от 245 000 руб.", image: "/pavilon-quiz/images/carlaspace.jpg" },
+  { id: "elefante", name: "Elefante", price: "от 565 000 руб.", image: "/pavilon-quiz/images/elefante.jpg" },
+  { id: "eleonora", name: "Eleonora", price: "от 520 000 руб.", image: "/pavilon-quiz/images/eleonora.jpg" },
 ];
 
 export const polycarbonateTypes = [
-  { id: "cellular", label: "Сотовый поликарбонат", image: "/images/cellular.jpg" },
-  { id: "monolithic", label: "Монолитный поликарбонат", image: "/images/monolithic.jpg" },
+  { id: "cellular", label: "Сотовый поликарбонат", image: "/pavilon-quiz/images/cellular.jpg" },
+  { id: "monolithic", label: "Монолитный поликарбонат", image: "/pavilon-quiz/images/monolithic.jpg" },
 ];
